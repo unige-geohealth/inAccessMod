0d43fc0ee6d4f3b6fa410c069c30611b8250b4bf
Pablo Timoner <Pablo.Timoner@unige.ch>
2022-09-14 15:34:02 +0200

    full hf table

--- R/create_hf_shapefile.R
+++ R/create_hf_shapefile.R
@@ -171,7 +171,7 @@ create_hf_shapefile <- function (mainPath, country, mostRecentBoundaries = TRUE,
     write.table(df[!inter[, 1]], paste(hfFolder, "coordinates_outside.txt", sep = "/"))
     message(paste("\nYou can access the removed HFs at:\n", paste(hfFolder, "coordinates_outside.txt", sep = "/"), "\n"))
   }
-  shp <- sf::st_as_sf(pts[inter[, 1], c("external_id", "workspace_id", "date", "MoSD3", "HFNAME")])
+  shp <- sf::st_as_sf(pts[inter[, 1], ])
   cat("\nSaving the HFs' shapefile...\n")
   sf::st_write(shp, paste(hfFolder, "health_facilities.shp", sep = "/"), append = FALSE)
   inputFolder <- stringr::str_extract(hfFolder, "scenario[0-9]{3}/[0-9]{14}")
