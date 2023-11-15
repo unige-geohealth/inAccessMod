d0ff6af4733a0867e98ff8b3109a4a292adf4cc9
Pablo Timoner <Pablo.Timoner@unige.ch>
2023-11-15 12:21:40 +0100

    <<-

--- inst/tests/testthat/test-initiate_project.R
+++ inst/tests/testthat/test-initiate_project.R
@@ -1,5 +1,5 @@
-temp_dir <- tempdir()
-country <- "Switzerland" # 168
+temp_dir <<- tempdir()
+country <<- "Switzerland" # 168
 test_that("test initiate_project", {
   expect_equal(initiate_project(temp_dir, testMode = TRUE), TRUE)
   expect_true(file.exists(file.path(temp_dir, country, 'data', 'config.txt')))
