db910fbbdde3454e01557e914160fcb47c7f4db2
Pablo Timoner <Pablo.Timoner@unige.ch>
2024-09-24 12:57:14 +0200

    adapt test download osm

--- tests/testthat/test-07-download_osm.R
+++ tests/testthat/test-07-download_osm.R
@@ -1,14 +1,11 @@
 test_that("test download_osm", {
-  expect_equal(download_osm(x = "roads", mainPath = temp_dir, country = country, alwaysDownload = TRUE, 
-                            countryName = TRUE, 
+  expect_equal(download_osm(mainPath = temp_dir, location = country, type = "roads", alwaysDownload = TRUE, 
                             mostRecent = TRUE, 
                             defaultClasses = TRUE), TRUE)
-  expect_equal(download_osm(x = "waterLines", mainPath = temp_dir, country = country, alwaysDownload = TRUE, 
-                            countryName = TRUE, 
+  expect_equal(download_osm(mainPath = temp_dir, location = country, type = "waterLines", alwaysDownload = TRUE, 
                             mostRecent = TRUE, 
                             defaultClasses = TRUE), TRUE)
-  expect_equal(download_osm(x = "naturalPolygons", mainPath = temp_dir, country = country, alwaysDownload = TRUE, 
-                            countryName = TRUE, 
+  expect_equal(download_osm(mainPath = temp_dir, country = country, type = "waterPolygons", alwaysDownload = TRUE, 
                             mostRecent = TRUE, 
                             defaultClasses = TRUE), TRUE)
 })
